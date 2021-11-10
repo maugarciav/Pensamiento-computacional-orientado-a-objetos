0409ecd85d33ab8a169594cebc4ebf206a89d52c
maugarciav <93749475+maugarciav@users.noreply.github.com>
2021-11-10 07:14:24 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout << "Hello world" << endl;
-  return 0;
+class Rectangulo{
+public:
+    double calcArea();
+    void setLargo(double la);
+    void setAncho(double an);
 
+private:
+    double largo, ancho;
+};
+
+double Rectangulo :: calcArea() {
+    return largo * ancho;
 }
+void Rectangulo :: setLargo(double la) {
+    largo = la;
+}
+void Rectangulo :: setAncho(double an) {
+    ancho = an;
+}
+
+
+int main(){
+    Rectangulo rect;
+    double area, l, a;
+
+    cin >> l;
+    cin >> a;
+
+    rect.setLargo(l);
+    rect.setAncho(a);
+    area = rect.calcArea();
+    cout << "El area es: " << area << endl;
+
+    return 0;
+}
+
+
+
+
+
