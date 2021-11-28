ad5b878fd910d85cb5de8bbbfbd557d5aa2c0ffe
maugarciav <93749475+maugarciav@users.noreply.github.com>
2021-11-28 04:19:51 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,13 +5,6 @@
  */
 
 
-/*
- * Comentarios:
- * En Torneo.h, al mandar a llamar el metodo confirmacion y querer registar un
- * nombre + espacio se crashea, intente usar getline(), pero no me funciono, aun estoy
- * investigadno como hacerlo.
- */
-
 #include <iostream>
 #include "Torneo.h"
 
@@ -24,75 +17,152 @@ void menu() {
     cout << "1. Registrar Jugador \n";
     cout << "2. Registrar Publico \n";
     cout << "3. Registrar Staff \n";
-    cout << "4. Ver lista de Jugadores \n";
-    cout << "5. Ver lista de Publcio \n";
-    cout << "6. Ver lista de Staff \n";
-    cout << "7. Salir \n";
+    cout << "4. Registrar Equipo \n";
+    cout << "5. Ver Jugadores registrados\n";
+    cout << "6. Ver Publico registrado\n";
+    cout << "7. Ver Staff registrado\n";
+    cout << "8. Ver Equipos registrados \n";
+    cout << "9. Buscar Jugador \n";
+    cout << "10. Buscar persona en el Publico \n";
+    cout << "11. Buscar persona en Staff \n";
+    cout << "12. Actualizar ranking de Equipo \n";
+    cout << "13. Agregar horas de trabajo para Staff \n";
+    cout << "14. Mostrar horas trabajadas Staff \n";
+    cout << "15. Salir \n";
+
 }
 
 int main(){
     Torneo j1;
     Torneo p1;
     Torneo s1;
+    Torneo e1;
 
     int opcion = 0;
-    string nom, area;
-    int id_edad;
+    string nom, area, coach, co;
+    int id, edad, ranking;
+    float horas;
     bool continua = true;
 
-    while (continua == true){
+    while (continua == true) {
+        cout << endl;
         menu();
         cout << endl << "Opcion: ";
         cin >> opcion;
 
-        if (opcion == 1){
+        if(opcion == 1){
             cout << "Nombre: ";
             cin >> nom;
             cout << "ID: ";
-            cin >> id_edad;
-            j1.agregar_jugador(nom, id_edad);
-        }
+            cin >> id;
+            cout << "¿Has estado en contacto con alguien con Covid en "
+                    "los ultimos 14 dias? si/no: ";
+            cin >> co;
+            if (co == "si"){
+                j1.registrar_jugador(nom, id, true);
+            }
+            else{
+                j1.registrar_jugador(nom, id, false);
+            }
 
-        else if (opcion == 2){
+        }
+        else if(opcion == 2){
             cout << "Nombre: ";
             cin >> nom;
             cout << "Edad: ";
-            cin >> id_edad;
-            p1.agregar_publico(nom, id_edad);
+            cin >> edad;
+            cout << "¿Has estado en contacto con alguien con Covid en "
+                    "los ultimos 14 dias? si/no: ";
+            cin >> co;
+            if(co == "si"){
+                p1.registar_publico(nom, edad, true);
+            }
+            else{
+                p1.registar_publico(nom, edad, false);
+            }
 
         }
-        else if (opcion == 3){
+        else if(opcion == 3){
             cout << "Nombre: ";
             cin >> nom;
             cout << "Area: ";
             cin >> area;
-            cout << "ID: ";
-            cin >> id_edad;
-            s1.agregar_staff(nom,area, id_edad);
-
-
+            cout << "Horas Trabajadas: ";
+            cin >> horas;
+            cout << "¿Has estado en contacto con alguien con Covid en "
+                    "los ultimos 14 dias? si/no: ";
+            cin >> co;
+            if(co == "si"){
+                s1.registar_staff(nom, area, horas, true);
+            }
+            else{
+                s1.registar_staff(nom, area, horas, false);
+            }
         }
-        else if (opcion == 4){
-            j1.lista_jugadores();
+        else if(opcion == 4){
+            cout << "Nombre: ";
+            cin >> nom;
+            cout << "Coach: ";
+            cin >> coach;
+            cout << "Ranking: ";
+            cin >> ranking;
+            e1.registrar_equipo(nom, coach, ranking);
         }
-
-        else if (opcion == 5){
-            p1.lista_publico();
+        else if(opcion == 5){
+            j1.ver_jugadores();
         }
-
-        else if (opcion == 6){
-            s1.lista_staff();
+        else if(opcion == 6){
+            p1.ver_publico();
         }
-
         else if(opcion == 7){
+            s1.ver_staff();
+        }
+        else if(opcion == 8){
+            e1.ver_equipos();
+        }
+        else if(opcion == 9){
+            cout << "Nombre del Jugador: ";
+            cin >> nom;
+            j1.buscar_jugador(nom);
+        }
+        else if(opcion == 10){
+            cout << "Nombre de la Persona: ";
+            cin >> nom;
+            p1.buscar_publico(nom);
+        }
+        else if(opcion == 11){
+            cout << "Nombre de Staff: ";
+            cin >> nom;
+            s1.buscar_staff(nom);
+        }
+        else if(opcion == 12){
+            cout << "Equipo: ";
+            cin >> nom;
+            cout << "Nuevo ranking: ";
+            cin >> ranking;
+            e1.actualizar_ranking(nom, ranking);
+        }
+        else if(opcion == 13){
+            cout << "Nombre: ";
+            cin >> nom;
+            cout << "Tiempo: ";
+            cin >> horas;
+            s1.agregar_horas(nom, horas);
+        }
+        else if (opcion == 14){
+            cout << "Nombre de Staff: ";
+            cin >> nom;
+            s1.ver_horas(nom);
+        }
+        else if (opcion == 15){
             continua = false;
-            cout << "ADIOS" << endl;
-        }
+            cout << "Adios" << endl;
 
+        }
         else{
-            cout << "Opcion invalida" << endl;
+            cout << "Opcion invalida";
         }
-
 
     }
+    return 0;
 }
